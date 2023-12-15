b19b91b30ae8647506a18aea45ed1aa756284b7e
Inetiger <ivensen18@gmail.com>
2023-12-15 12:55:06 +0100

    Add project files.

---
+++ Filmer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parprogrammering_Arthur
+{
+    internal class Filmer
+    {
+        private string _title;
+        private int _releaseYear;
+        private string _description;
+        private string _director;
+        private string _actors;
+
+        public Filmer(string title, int releaseYear, string description, string director, string actors)
+        {
+            _title = title;
+            _releaseYear = releaseYear;
+            _description = description;
+            _director = director;
+            _actors = actors;
+        }
+
+        public void PrintInfoOnMovie()
+        {
+            Console.WriteLine($"Tittle: {_title}\nRelease Year: {_releaseYear}\nDescription: {_description}\nDirector: {_director}\nActors: {_actors}");
+        }
+
+        public string PrintTitle()
+        {
+            return _title;
+        }
+    }
+}
